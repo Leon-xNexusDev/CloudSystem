8f1a8633f5538afb1df3e9add91b632d81e2adfc
Gamer-Bjoern <107365905+Gamer-Bjoern@users.noreply.github.com>
2022-07-24 16:18:25 +0200

    added some cloud help messages

--- utils/CheckInput.go
+++ utils/CheckInput.go
@@ -31,11 +31,24 @@ func CheckInput() {
 	opt, _ := getInput(White+userName.Username+Reset+"@"+Red+hostName+Blue+" » ", reader)
 
 	switch opt {
+	case "cloud help", "cloud":
+		fmt.Println(Green + "Help" + Reset + " » " + Blue + "cloud help (shows this help)" + Reset)
+		fmt.Println(Green + "Help" + Reset + " » " + Blue + "cloud version (shows the version of the CloudSystem)" + Reset)
+		fmt.Println(Green + "Help" + Reset + " » " + Blue + "cloud info (shows the info of the CloudSystem)" + Reset)
+		fmt.Println(Green + "Help" + Reset + " » " + Blue + "cloud stop (stops the CloudSystem)" + Reset)
+		fmt.Println(Green + "Help" + Reset + " » " + Blue + "task help (shows the help of the task command)" + Reset)
 	case "cloud stop":
 		fmt.Println("Stopping Cloud...")
 		os.Exit(0)
-	case "cloud help", "cloud":
-		fmt.Println(Green + "Help" + Reset + " » " + "cloud stop")
+	case "cloud version":
+		fmt.Println("CloudSystem v1.0 alpha")
+	case "cloud info":
+		fmt.Println("Version v1.0 alpha")
+		fmt.Println("Author: Leon-xNexusDev & Gamer_Bjoern")
+		fmt.Println("Discord: https://discord.gg/QWQWQWQ") //TODO Add discord link
+		fmt.Println("License: GNU GPLv3")
+		fmt.Println("Copyright: 2022")
+		fmt.Println("All rights reserved.")
 	/*case "your name":
 	name, _ := getInput(">>", reader)
 	fmt.Println(name)*/
